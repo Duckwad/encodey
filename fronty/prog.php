dddfb726cab7be3c91a15104be1bb7ddff7bb2fd
shinigamibob <kmarasinghe1@gmail.com>
2013-08-08 19:54:13 -0600

    Refreshes progress bar, queue, and file listings after each encode

--- fronty/prog.php
+++ fronty/prog.php
@@ -5,6 +5,19 @@
 	flush();
 	$info = shell_exec('cat '.$basedir.'progress.log'); //reads progress file
 	
+	if (strpos($info,"FINISHED") !==false){
+		echo "<script type='text/javascript'>
+		$('#bardivs').empty();
+		$('#filelist').load('exec.php?q=dir');
+		setTimeout(function() {
+			$('#update').empty();
+			$('#ed-frame').attr('src', $('#ed-frame').attr('src'));
+			$('completed').load('exec.php?q=dircomp');
+			return false;
+			}, 2500);
+		</script>";
+}
+	
 	if (empty($info))
 		{
 			echo "No encode in progress";
@@ -22,10 +35,10 @@
 		$info = substr($info,$end+1);
 		}
 	
-	$loc[1] = substr($loc[1],0,4);
+	$loc[1] = substr($loc[1],0,-1);
 	echo $loc[1].' - '.$loc[0];
 	$bar1 = '$(function() {$("#enc-progress").progressbar({value:'.$loc[1].'});});';
-	
+		
 	end:
 ?>
 <script type="text/javascript">
